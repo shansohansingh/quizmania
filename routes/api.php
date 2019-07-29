5b0fac8a2a8690e398345c265364ebf6e4927689
aswal94 <aswal.amit94@gmail.com>
2019-07-29 19:55:29 +0530

    API Fixes

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 | API Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register API routes for your application. These
+| Here is where you c                   an register API routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
@@ -17,8 +17,15 @@ use Illuminate\Http\Request;
 //     return $request->user();
 // });
 
-Route::post('login', 'API\UserController@login');
-Route::post('register', 'API\UserController@register');
-Route::group(['middleware' => 'auth:api'], function(){
-Route::post('details', 'API\QuestionAnswerController@details');
+Route::prefix('v1')->group(function () { 
+    
+    /* Authentication */
+    Route::post('login', 'API\UserController@login');
+    Route::post('register', 'API\UserController@register');
+
+
+    /* token middleware */
+    Route::group(['middleware' => 'auth:api'], function(){
+        Route::post('details', 'API\QuestionAnswerController@details');
+    });
 });
